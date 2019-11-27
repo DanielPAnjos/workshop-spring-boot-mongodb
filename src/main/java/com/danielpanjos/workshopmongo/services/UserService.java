a2b5941c65d895f97d56cd367138592731521841
Daniel Pombo <pombo.daniel@gmail.com>
2019-11-27 12:04:27 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/danielpanjos/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.danielpanjos.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.danielpanjos.workshopmongo.domain.User;
+import com.danielpanjos.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll() {
+		return repo.findAll();
+	}
+}
